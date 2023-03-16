d1c31b7b3a781e861fc3e87cc48ab59326ce459f
faustine-van <faustinemuhayemariya44@gmail.com>
2023-03-16 23:16:12 +0000

    display using puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
+/**
+ *main-print a code of line using printf function
+ *
+ *Return: 0
+ */
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is ia piece of art,");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
