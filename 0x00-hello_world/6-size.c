17eae3a6ba8b37034b8ce1b02b2992b16125556b
faustine-van <faustinemuhayemariya44@gmail.com>
2023-03-16 23:36:09 +0000

    display using byte of each data type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
-
+/**
+ *main - sizeof()function to show byte of datatype
+ *
+ *Return: 0
+ */
 int main(void)
 {
 
-	printf("Size of a char: %lx byte(s)\n", sizeof(char));
-	printf("Size of an int: %ld byte(s)\n", sizeof(unsigned long int));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(unsigned long int));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(unsigned long int));
-	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
