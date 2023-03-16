3cd7a713b37c14bbfbd09d42975d6c1796b8ac7a
faustine-van <faustinemuhayemariya44@gmail.com>
2023-03-16 23:52:37 +0000

    print line to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,13 @@
 #include<stdio.h>
+/**
+ *main - using fprintf to print error message
+ *
+ *Return: 1
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
+
