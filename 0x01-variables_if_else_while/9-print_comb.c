a26d40b4bf9bd98235d54773e0c8dc1e72deea78
faustine-van <faustinemuhayemariya44@gmail.com>
2023-03-18 01:53:21 +0000

    print numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,12 @@ int main(void)
 {
 	int a;
 
-	for (a = '0'; a <='9'; a++)
+	for (a = '0'; a <= '9'; a++)
 	{
 		putchar(a);
+		putchar(',');
+		putchar(' ');
 	}
-	putchar(',');
-	putchar(' ');
 	putchar('\n');
 	return (0);
 }
