d0bc1a62758f6ac1e04a561e8af83d3e0bff7961
faustine-van <faustinemuhayemariya44@gmail.com>
2023-03-21 23:26:14 +0000

    updated

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *add - add two number
+ *@num1: num1 to specify number add
+ *@num2: num2 to specify
+ *Return: result = num1 + num2
+*/
+int add(int num1, int num2)
+{
+	int result;
+
+	result = num1 + num2;
+	return (result);
+}
