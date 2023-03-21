a8b37e555e1b1f75e8c0e98b748980f739cf0adb
faustine-van <faustinemuhayemariya44@gmail.com>
2023-03-21 15:55:54 +0000

    print alphabets ten time

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,20 @@
 #include "main.h"
 /**
- *main - entry :
+ * print_alphabet_x10 - write the the lower-case alphabet on a line.
  *
- *Return: 0
-*/
-void print_alphabet_x10(void);
+ */
+void print_alphabet_x10(void)
 {
 	char c;
+	int i;
 
-	for (c = 'a' ; c <= 'z' ; c++)
+	for (i = 0; i < 10 ; i++)
 	{
-		_putchar(c);
+		for (c = 'a' ; c <= 'z' ; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
-
 }
+
