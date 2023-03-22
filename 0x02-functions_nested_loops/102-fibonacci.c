6701457129e87445cbb6e35c75a491635816d208
faustine-van <faustinemuhayemariya44@gmail.com>
2023-03-22 23:24:27 +0000

    updated

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,22 +12,25 @@ int main(void)
 	int count = 3;
 	long int first = 1, second = 2;
 	long int next = first + second;
-
+	
 	printf("%lu, ", first);
 	printf("%lu, ", second);
+
 	while (count <= 50)
 	{
 		if (count == 50)
 		{
-			printf("%lu \n", next)
-				else
-				{
-					printf("%lu, ", next);
-				}
-				first = second;
-				second = next;
-				next = first + second;
-				count++;
+			printf("%lu \n", next);
+		}
+		else
+		{
+			printf("%lu, ", next);
+		}
+		first = second;
+		second = next;
+		next = first + second;
+		count++;
 	}
-		return (0);
+	return (0);
 }
+
