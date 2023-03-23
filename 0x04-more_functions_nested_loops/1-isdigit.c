91b6edfb738fcb6e6124f60a218d5088b958d5ad
faustine-van <faustinemuhayemariya44@gmail.com>
2023-03-23 12:07:15 +0000

    updated

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <ctype.h>
 
 /**
  * _isdigit - uppercase letters
@@ -10,7 +11,7 @@
 
 int _isdigit(int c)
 {
-	if (c == 48 && c <= 57)
+	if (isdigit(c))
 		return (1);
 	else
 		return (0);
