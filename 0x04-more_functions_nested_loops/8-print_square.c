c8bb44886fd98a466b745dafc9e96c45e300d8ec
faustine-van <faustinemuhayemariya44@gmail.com>
2023-03-23 19:32:09 +0000

    updated

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,18 +8,21 @@
  */
 void print_square(int size)
 {
+	int k, i;
+
 	if (size <= 0)
 	{
 		_putchar('\n');
 	} else
 	{
-		int i;
-
 		for (i = 0; i < size; i++)
 		{
-			_putchar('#');
+			for (k = 0; k < size; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 
 }
