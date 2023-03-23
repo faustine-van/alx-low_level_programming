a208ebbceb64f5a8d53b9b3c0e17f5568a05d7dc
faustine-van <faustinemuhayemariya44@gmail.com>
2023-03-23 18:56:44 +0000

    updated

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - draw a square
+ * @size: character to check
+ *
+ * Return: 0 or 1
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i;
+		for (i = 0; i < size; i++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+
+}
