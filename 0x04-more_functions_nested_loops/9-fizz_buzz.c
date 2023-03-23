fd6768c98252f68678c9d5bba546dd448b3b2b83
faustine-van <faustinemuhayemariya44@gmail.com>
2023-03-23 20:12:56 +0000

    updated

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+	for (i = 1; i <= 100; i++)
+	{
+		 if (i % 3 == 0)
+		 {
+			 printf("%s" ,"Fizz ");
+		 } else if (i % 5 == 0)
+		 {
+			 printf("%s" ,"Buzz ");
+		 } else if (i % 5 == 0 && i % 3 == 0)
+		 {
+			 printf("%s" ,"FizzBuzz ");
+		 }
+		 else
+		 {
+			 printf("%d ", i);
+		 }
+
+	}
+	printf("\n");
+	return (0);
+}
