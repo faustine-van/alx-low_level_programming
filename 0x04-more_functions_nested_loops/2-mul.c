7c611a2e49079f27e45f0e9d1a42810345951861
faustine-van <faustinemuhayemariya44@gmail.com>
2023-03-23 12:17:03 +0000

    updated

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *mul - multiplies two integers
+ *@a: one varible to be tested
+ *@b: one varible to be tested
+ *Return: 0 or 1
+*/
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return result;
+}
