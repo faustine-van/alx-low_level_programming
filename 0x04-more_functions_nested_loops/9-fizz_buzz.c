f4b4365a49f1685dbcbcd9b4c4bc7e4a12f3df97
faustine-van <faustinemuhayemariya44@gmail.com>
2023-03-23 20:21:36 +0000

    updated

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,23 +9,25 @@
 int main(void)
 {
 	int i;
+
 	for (i = 1; i <= 100; i++)
 	{
-		 if (i % 3 == 0)
-		 {
-			 printf("%s" ,"Fizz ");
-		 } else if (i % 5 == 0)
-		 {
-			 printf("%s" ,"Buzz ");
-		 } else if (i % 5 == 0 && i % 3 == 0)
-		 {
-			 printf("%s" ,"FizzBuzz ");
-		 }
-		 else
-		 {
-			 printf("%d ", i);
-		 }
+		if (i % 3 == 0)
+		{
+			printf("%s ", "Fizz");
 
+		} else if (i % 5 == 0)
+		{
+			printf("%s ", "Buzz");
+		}
+		else if (i % 5 == 0 && i % 3 == 0)
+		{
+			printf("%s ", "FizzBuzz");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
 	}
 	printf("\n");
 	return (0);
