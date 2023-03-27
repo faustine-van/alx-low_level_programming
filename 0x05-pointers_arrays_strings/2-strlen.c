c9810342ca91a962195b58c2cecbbca5bd86f567
faustine-van <faustinemuhayemariya44@gmail.com>
2023-03-27 09:37:01 +0000

    updated

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strlen - number of length
+ *@s: pointer variable to be tested
+ *strlen: length of pointer
+ *Return: length of variable
+*/
+int _strlen(char *s)
+{
+	char r;
+
+	r = strlen(s);
+	return (r);
+}
