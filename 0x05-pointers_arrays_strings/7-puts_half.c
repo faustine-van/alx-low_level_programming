4676f9b605594a36d6a9b8e4f65fbc4131a77530
faustine-van <faustinemuhayemariya44@gmail.com>
2023-03-27 22:27:58 +0000

    updated

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #define MAXSTRING 446
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 /**
  *puts_half - print half of string
  *@str: variable to print second half string
@@ -8,20 +10,24 @@
 */
 void puts_half(char *str)
 {
-	char first_half_string[100], last_half_string[100];
-	int count, len, half;
+	int len_str, count;
 
-	len = strlen(str);
-	half = len / 2;
+	len_str= strlen(str);
 
-	for (count = 0; count < half; count++)
+	if(len_str % 2 != 0)
 	{
-		first_half_string[100] = str[count];
+		count = (len_str / 2) + 1;
 	}
-	first_half_string[100] = '\0';
-
-	for (count = half; count <= len; count++)
+	else
 	{
-		_putchar(str[count]);
+		count = (len_str / 2);
 	}
+	while (count < len_str)
+	{
+		_putchar(*(str + count));
+		count++;
+
+	}
+	_putchar('\n');
+	
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -11,5 +11,6 @@ void print_array(int *a, int n);
 void rev_string(char *s);
 char *_strcpy(char *dest, char *src);
 void puts_half(char *str);
+void rev_string(char *s);
 
 #endif
