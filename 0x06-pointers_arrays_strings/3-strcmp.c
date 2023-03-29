2cab1f1887588b44ad24f86695f4874c961e36b2
faustine-van <faustinemuhayemariya44@gmail.com>
2023-03-29 18:47:11 +0000

    updated

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,16 +2,16 @@
 #include <string.h>
 /**
  *_strcmp - compare string of s1 and s2;
- *@s1: 
- *@s2:
- *Return:
+ *@s1:pointer to be tested
+ *@s2: pointer to be tested;
+ *Return: return result
 */
 int _strcmp(char *s1, char *s2)
 {
 	int i;
 	int result;
 
-	for (i = 0; *(s1 + i) == *(s2 + i) && *(s1 + i) == '\0';);
+	for (i = 0; *(s1 + i) == *(s2 + i) && *(s1 + i) == '\0';)
 
 	if (*(s1 + i) < *(s2 + i))
 	{
