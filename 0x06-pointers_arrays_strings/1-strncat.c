e9673ef4a70386f42a26d5c0e25a414785363e2c
faustine-van <faustinemuhayemariya44@gmail.com>
2023-03-30 07:59:51 +0000

    updated

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,24 +1,26 @@
 #include "main.h"
 #include <string.h>
 /**
- *_strncat - append two string 
+ *_strncat - append two string
  *@dest: to be tested
  *@src: to be tested
+ *@n: size of bytes to be added to dest from src
  *Return: dest;
 */
 char *_strncat(char *dest, char *src, int n)
 {
 	int a, b, len;
-
+	/* length of dest*/
 	len = strlen(dest);
 	for (a = 0; a < n && dest[a] != '\0'; a++)
 		;
 
+	/* take byte from src and added to dest */
 	for (b = 0; b < n && src[b] != '\0'; b++)
 	{
 		dest[len + b] = src[b];
 		a++;
 	}
 	dest[len + a] = '\0';
-	return dest;
+	return (dest);
 }
