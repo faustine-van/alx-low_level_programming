b062b1f670406da493f532c3e497746a65b4931e
faustine-van <faustinemuhayemariya44@gmail.com>
2023-04-02 19:23:15 +0000

    updated

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,12 +14,10 @@ int _atoi(char *s)
 	unsigned int value = 0;
 	int current;
 
-	
-	if (s[0]  == '-')
+	if (s[i] == '-')
 	{
 		value *= -1;
 	}
-		
 	while (s[i] != '\0')
 	{
 		if (isdigit(s[i]))
@@ -29,41 +27,5 @@ int _atoi(char *s)
 		}
 		i++;
 	}
-		
-	return value;
-
-	/*int a;
-	int sig, start;
-	unsigned int num;
-
-	if (s[0] == '-')
-	{
-		sig = -1;
-	}
-	if (sig == -1)
-	{
-		start = 1;
-	}
-	else
-	{
-		start = 0;
-	}
-	num = 0;
-	
-	for (a = start; s[a] != '\0'; a++)
-	{
-			if (isdigit(s[a]))  the same as if(*s >= 48 && *s <= 57)*/
-				/*num = num * 10 + s[a] - 48;
-	}
-	if (sig == -1)
-	{
-		num *= -1;
-	} 
-	else
-	{
-		 num = num;
-	}
-
-	return (num);*/
-
+	return (value);
 }
