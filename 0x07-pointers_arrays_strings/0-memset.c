8328e806f7eab22fccb5e576af3dbc3ac52559ed
faustine-van <faustinemuhayemariya44@gmail.com>
2023-04-03 11:52:21 +0000

    updated

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#define BUFFER_SIZE 20
+/**
+ *_memset - fill memory with constant byte b
+ *@s: to be tested
+ *@b: constant byte variable
+ *@n: size of n;
+ *Return: s
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	while  (n--)
+	{
+		*s++  = (unsigned char)b;
+	}
+	return (s);
+}
