514e68edc1f95a0bc9f3ee91f724a38f324694c9
faustine-van <faustinemuhayemariya44@gmail.com>
2023-04-08 17:26:25 +0000

    updated

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - print multplication of number
+ *@argv: an array of size of argc
+ *@argc: count number of arguments
+ *Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int a, mul;
+	mul = 1;
+	if (argc > 1)
+	{
+		for (a = 1; a < argc; a++)
+		{
+			mul *= atoi(argv[a]);
+
+		}
+		printf("%d\n", mul);
+	}
+	else
+		printf("Error\n");
+	return (1);
+}
