7c789a1f1db13037f3150e3fcf5b35c778f3e352
faustine-van <faustinemuhayemariya44@gmail.com>
2023-04-11 09:18:34 +0000

    dynamic allocation

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,9 +12,9 @@ char *create_array(unsigned int size, char c)
 	char *str;
 	unsigned int  a;
 
-	str = malloc(sizeof(char) * (size - 1));
+	str = malloc(sizeof(char) * (size));
 
-	if (size == 0)
+	if (str == 0)
 	{
 		return (NULL);
 	}
