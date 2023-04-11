da347e6fa3f904e784417a325893252ba3f4ad35
faustine-van <faustinemuhayemariya44@gmail.com>
2023-04-11 23:09:05 +0000

    updated

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ *alloc_grid - allocate space for return pointer to 2d dimension
+ *@width: to be used
+ *@height: to be used
+ *Return: ptr
+*/
+int **alloc_grid(int width, int height)
+{
+	int **ptr, a, b;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(height * sizeof(int *));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (a = 0; a < height; a++)
+	{
+		ptr[a] = malloc(width * sizeof(int));
+
+		if (ptr[a] == NULL)
+		{
+			for (b = 0; b < a; b++)
+			{
+				free(ptr[a]);
+			}
+			free(ptr);
+			return (NULL);
+		}
+		for (b = 0; b < width; b++)
+		{
+			ptr[a][b] = 0;
+
+		}
+	}
+	return (ptr);
+}
