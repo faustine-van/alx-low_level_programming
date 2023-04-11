eaba93e09440ef23626ee80a64d710c567e302ff
faustine-van <faustinemuhayemariya44@gmail.com>
2023-04-11 11:34:54 +0000

    dynamic allocation

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+/**
+ *_strdup - print duplicated string using copy of allocated pointed by str
+ *@str: string to duplicated
+ *Return: ptr tha pointed by str
+*/
+char *_strdup(char *str)
+{
+	char *ptr;
+
+	int len = strlen(str) + 1;
+
+	ptr = malloc(len);
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	if (ptr != NULL)
+	{
+
+		/*strcpy(ptr, str);*/
+		int a = 0;
+
+		while (a < len)
+		{
+			ptr[a] = str[a];
+			a++;
+		}
+	}
+	else
+		return (NULL);
+
+	return (ptr);
+	free(str);
+}
