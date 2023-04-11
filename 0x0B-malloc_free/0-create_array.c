f179cab1b32866588587c277c0b599fff1ef6bcb
faustine-van <faustinemuhayemariya44@gmail.com>
2023-04-11 09:05:34 +0000

    dynamic allocation

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ *create_array - print array
+ *@size: size to be allocated
+ *@c: string to outputed
+ *Return: str pointer
+*/
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int  a;
+
+	str = malloc(sizeof(char) * (size));
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	a = 0;
+
+	while (a < size)
+	{
+		str[a] = c;
+		a++;
+	}
+	return (str);
+}
