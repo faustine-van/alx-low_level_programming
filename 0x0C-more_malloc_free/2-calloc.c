5cff4b9ac980cfae89889f00e7df10ebd4476382
faustine-van <faustinemuhayemariya44@gmail.com>
2023-04-13 19:36:38 +0000

    dynamic  allocation

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ *_calloc - allocate memory using malloc malloc have
+ *to work calloc function
+ *@nmemb: number of block
+ *@size: number of each blocK
+ *Return: ptr
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+		return (NULL);
+	return (ptr);
+}
