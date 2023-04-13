64d1488e195068ce0b705b643bc76a3d14dea861
faustine-van <faustinemuhayemariya44@gmail.com>
2023-04-13 23:56:25 +0000

    updated

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,17 +13,18 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
 
-
 	if (nmemb == 0 || size == 0)
+	{
 		return (NULL);
+	}
 
 	ptr = malloc(nmemb * size);
 
 
 	if (ptr == NULL)
 	{
-		exit(0);
 		return (NULL);
 	}
+		memset(ptr, 0, nmemb * size);
 	return (ptr);
 }
