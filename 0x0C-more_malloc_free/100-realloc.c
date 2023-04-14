6467583c811931a446e04c25a2ce17259c05fc69
faustine-van <faustinemuhayemariya44@gmail.com>
2023-04-14 20:56:07 +0000

    updated

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,11 +17,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-	if(new_size > old_size)
-	{
-		return (ptr);
-	}
-
 	if (ptr == NULL)
 	{
 		nptr = malloc(new_size);
@@ -44,7 +39,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 
 	return (nptr);
-	free(nptr);
 
 
 }
