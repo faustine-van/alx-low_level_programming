dcd54b41965b9f9ea7eece1ec4ef1f9c4f640187
faustine-van <faustinemuhayemariya44@gmail.com>
2023-04-18 09:09:40 +0000

    my_message

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+/**
+ *free_dog - free dog
+ *@d: ponter used to free
+*/
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
