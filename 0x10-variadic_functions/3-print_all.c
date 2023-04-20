83db5b9e6a6d7dd629669a04c45690ebe8d585cf
faustine-van <faustinemuhayemariya44@gmail.com>
2023-04-20 21:02:12 +0000

    my_message

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ *print_all - print anything such character , string , integers and float
+ *@format: format string
+*/
+void print_all(const char * const format, ...)
+{
+	int i;
+	char c;
+	float f;
+	char *s, *form;
+	va_list args;
+
+	va_start(args, format);
+	form = (char *) format; /*create nonconstant*/
+	while (*form != '\0')
+	{
+		switch (*form)
+		{
+			case 'c':
+				c = va_arg(args, int);
+				printf("%c", c);
+				break;
+			case 'i':
+				i = va_arg(args, int);
+				printf("%d", i);
+				break;
+			case 'f':
+				f = va_arg(args, double);
+				printf("%f", f);
+				break;
+			case 's':
+				s = va_arg(args, char*);
+				if (s != NULL)
+				{
+					printf("%s", s);
+				}
+				printf("(nil)");
+				break;
+			default:
+				/* do nothing if format character is not recognized */
+				break;
+		}
+		form++; /* increment the format string pointer */
+		if (*form != '\0' && (*form == 'c' || *form == 'i' || *form == 'f' || *form == 's'))
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+	va_end(args);
+}
