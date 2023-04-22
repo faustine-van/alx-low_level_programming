2983ffc278d751d76816ecb8ea5441255b1c454e
faustine-van <faustinemuhayemariya44@gmail.com>
2023-04-22 20:08:09 +0000

    updated testfiles

---
+++ 0x10-variadic_functions/test_files/3-main.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+#include <stddef.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_all(NULL, 'B', 3, NULL);
+    return (0);
+}
