8dcb26519d059ebff3ff5350e26597e2637560dc
faustine-van <faustinemuhayemariya44@gmail.com>
2023-04-27 23:42:29 +0000

    updated

---
+++ 0x04-more_functions_nested_loops/test_files/7-main.c
@@ -0,0 +1,16 @@
+julien@ubuntu:~/0x04$ cat 7-main.c
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_diagonal(0);
+    print_diagonal(2);
+    print_diagonal(10);
+    print_diagonal(-4);
+    return (0);
+}
