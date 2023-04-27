28ed686efa693c26f9de351c245c33a1ae077072
faustine-van <faustinemuhayemariya44@gmail.com>
2023-04-27 21:16:40 +0000

    updated

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,29 +3,41 @@
 #include <string.h>
 #include "lists.h"
 /**
- *add_node - print list od data
+ *add_node_end - print list od data
  *@head: struct
  *@str: string
  *Return: the address of the new element, or NULL if it failed
 */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
+	list_t *temp;
 
-	/*create new singled list node*/
+	/*create new  singled list node */
 	new = malloc(sizeof(list_t));
 
-
 	/* if unable to allocate memory for head node*/
+
 	if (new == NULL)
 		return (NULL);
 
-	/*insert new node at the beginning*/
 	new->str = strdup(str);
 	new->len = strlen(str);
-	new->next = *head;
+	new->next = NULL;
 
-	*head = new;
-
+	/*inserts at the end of the linked list*/
+	if (*head == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = new;
+	}
 	return (new);
 }
