d9906a96daa4b43b792cb5507fe378adc0356c56
faustine-van <faustinemuhayemariya44@gmail.com>
2023-04-27 20:08:06 +0000

    updated

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,7 +12,12 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 
-	new =  malloc(sizeof(list_t));
+	new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
 	new->str = strdup(str);
 	new->len = strlen(str);
 	new->next = *head;
@@ -21,6 +26,6 @@ list_t *add_node(list_t **head, const char *str)
 	if (new->next == NULL)
 	{
 		return (NULL);
-	}
+	}i
 	return (new->next);
 }
