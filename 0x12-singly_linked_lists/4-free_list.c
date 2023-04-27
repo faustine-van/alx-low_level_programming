4b332d33391046a3a3220d3a2fc57b33bc15b808
faustine-van <faustinemuhayemariya44@gmail.com>
2023-04-27 22:30:33 +0000

    updated

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ *free_list - free te list_t
+ *@head: point to the head
+ *Return nothing
+*/
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
