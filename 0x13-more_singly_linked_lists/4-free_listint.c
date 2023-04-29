60bd3d7440676b2aa1b10428c3c96b1c701a7dff
faustine-van <faustinemuhayemariya44@gmail.com>
2023-04-29 20:43:06 +0000

    updated

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -6,7 +6,12 @@
 /**
  *free_listint - frees a listint_t list.
  *@head: pointer to head of list node
- * Return: nothing
+ *
+ *Descrption: it cannot modify the head pointer itself, 
+ *and so it cannot set it to NULL after freeing the list.
+ *This function is useful if you do not need to modify the
+ *head pointer itself, and just need to free the nodes in the list.
+ *Return: nothing
  */
 void free_listint(listint_t *head)
 {
--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,11 +6,21 @@
 /**
  * free_listint2 - function that frees a listint_t list
  *@head: point to the listint_t lsi
- * The function sets the head to NULL
+ *Description: it cannot modify the head pointer itself,
+ *and so it cannot set it to NULL after freeing the list.
+ *This function is useful if you do not need to modify
+ *the head pointer itself, and just need to free the nodes in the list.
  */
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
+
+	/*checking if the head pointer itself is NULL*/
+	/*or if the node it's pointing to is NULL.*/
+	if (head == NULL && *head == NULL)
+	{
+		return;
+	}
 
 	while (*head != NULL)/*traverse entire list.*/
 	{
