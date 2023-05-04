c26fdd77423432efca86259f4a3139ca7b94bff1
faustine-van <faustinemuhayemariya44@gmail.com>
2023-05-04 10:13:55 +0000

    print binary number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_binary - print binary number
+ *@n: value to be printed in binary
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+	int a;
+	int rem_zero = 1;
+
+	for (a = 31; a >= 0; a--)
+	{
+		if (n & (1 << a))
+		{
+			rem_zero = 0;
+			putchar ('1');
+		}
+		else if (!rem_zero)
+		{
+			putchar('0');
+		}
+	}
+	if (rem_zero)
+	{
+		putchar('0');
+	}
+}
