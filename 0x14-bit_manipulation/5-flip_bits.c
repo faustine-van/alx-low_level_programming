8bc2cf674608f0995d74ad5f66fdb12c8465f134
faustine-van <faustinemuhayemariya44@gmail.com>
2023-05-04 19:56:03 +0000

    updated

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -13,7 +13,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	int count = 0;
 	int res = n ^ m;
 
-	while (res)
+	while (res != 0)
 	{
 		count += (res & 1);/*res = res & (res - 1);*/
 		res >>= 1;/*count++;*/
