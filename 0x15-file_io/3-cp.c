e1f701eb43569331520bad0c112e0843d16e7a57
faustine-van <faustinemuhayemariya44@gmail.com>
2023-05-08 23:26:43 +0000

    copy content of file to Another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,11 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <fcntl.h>
-#include <string.h>
-#define BUFFER_SIZE 1024
+#include "main.h"
 /**
  * main - copy content of one file to another file
  * @ac: number of argument
@@ -14,7 +7,7 @@
  */
 int main(int ac, char **av)
 {
-	int t_file, f_file;
+	int t_file, f_file, read_file;
 	char buffer[BUFFER_SIZE];
 
 	if (ac != 3)
@@ -28,28 +21,35 @@ int main(int ac, char **av)
 		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	t_file = open(av[2], O_WRONLY | O_TRUNC,
+	t_file = open(av[2], O_WRONLY | O_CREAT | O_TRUNC,
 			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (t_file == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-	while (read(f_file, buffer, BUFFER_SIZE))
+	while ((read_file = read(f_file, buffer, BUFFER_SIZE)) > 0)
 	{
-		write(t_file, buffer, strlen(buffer));
-		if (read(f_file, buffer, BUFFER_SIZE) == -1)
+		if (write(t_file, buffer, read_file) != read_file)
 		{
-			dprintf(2, "Error: Can't write to %s", av[2]);
+			dprintf(2, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
 	}
-	if (close(t_file) == -1)
+	if (read_file == -1)
 	{
-		dprintf(2, "Error: Can't close fd %i", t_file);
+		dprintf(2, "Error: Can't read from file  %s", av[1]);
+		exit(98);
+	}
+	if (close(f_file) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", f_file);
 		exit(100);
 	}
-	close(f_file);
-	close(t_file);
-	return (1);
+	if (close(t_file) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", t_file);
+		exit(100);
+	}
+	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#define BUFFER_SIZE 1024
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void print_error(const char __attribute__((unused))*message, 
+		const char __attribute__((unused))*filename, 
+		int __attribute__((unused)) fd);
 #endif
