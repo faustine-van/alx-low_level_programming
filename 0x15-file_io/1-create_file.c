e5362f941040f323f59233371091e243dd90cf42
faustine-van <faustinemuhayemariya44@gmail.com>
2023-05-08 19:20:41 +0000

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,19 +19,17 @@ int create_file(const char *filename, char *text_content)
 	int create_file, write_content;
 	/*char c[BUFFER_SIZE];*/
 
-
-	create_file = creat(filename, S_IRUSR | S_IWUSR);
-
 	if (filename == NULL)
 		return (-1);
 
-	if (create_file < 0)
+	create_file = open(filename, O_WRONLY | O_CREAT | O_TRUNC,
+			S_IRUSR | S_IWUSR);
+	if (create_file == -1)
+		return (-1);
+
+	if (text_content == NULL)
 	{
 		return (-1);
-	}
-	else if (text_content == NULL)
-	{
-		creat(filename, S_IRUSR | S_IWUSR);
 	}
 	else
 	{
@@ -44,5 +42,5 @@ int create_file(const char *filename, char *text_content)
 		}
 	}
 	close(create_file);
-	return (create_file);
+	return (1);
 }
