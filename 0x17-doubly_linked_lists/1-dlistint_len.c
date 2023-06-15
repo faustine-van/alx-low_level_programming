9376eee273a3cdef427fe2f25d6de70a1a607043
faustine-van <faustinemuhayemariya44@gmail.com>
2023-06-15 18:29:52 +0000

    updatef

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -13,12 +13,10 @@ size_t dlistint_len(const dlistint_t *h)
 {
 	size_t size = 0;
 
-	if (h == NULL)
-		return (0);
 	while (h != NULL)
 	{
-		size++;
 		h = h->next;
+		size++;
 	}
 	return (size);
 }
