7becd1c501ffcf67259000e04a37e187d6ee1d6c
faustine-van <faustinemuhayemariya44@gmail.com>
2023-07-10 19:33:39 +0000

    add an e;ement tp the hash tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,8 +13,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned int index;
 	hash_node_t *currentNode, *newNode;
 
-	if (!ht || !key || !value)
-		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
 
